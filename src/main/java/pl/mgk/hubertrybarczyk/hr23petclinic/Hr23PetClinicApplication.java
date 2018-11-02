962e288a47b6917f49a0dc1439088a42d7a68eac
Number23 <vert3r@gmail.com>
2018-11-02 16:04:29 +0100

    Initial Project Add

---
+++ src/main/java/pl/mgk/hubertrybarczyk/hr23petclinic/Hr23PetClinicApplication.java
@@ -0,0 +1,12 @@
+package pl.mgk.hubertrybarczyk.hr23petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hr23PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hr23PetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/mgk/hubertrybarczyk/hr23petclinic/Hr23PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mgk.hubertrybarczyk.hr23petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hr23PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
