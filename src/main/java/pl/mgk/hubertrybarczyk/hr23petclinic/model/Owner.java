4d1e81991a8d5949f0b2b4874f4c2d216095f6d1
Number23 <vert3r@gmail.com>
2018-11-02 19:32:36 +0100

    Adding POJOs.

---
+++ src/main/java/pl/mgk/hubertrybarczyk/hr23petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package pl.mgk.hubertrybarczyk.hr23petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/pl/mgk/hubertrybarczyk/hr23petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package pl.mgk.hubertrybarczyk.hr23petclinic.model;
+
+public class Vet extends Person {
+}
